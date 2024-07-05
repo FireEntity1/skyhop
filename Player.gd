e40eed46fa6155824bb0817f0ef00f538a6757ca
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-07-05 15:54:46 -0600

    Basic movement, skybox, platform

--- Player.gd
+++ Player.gd
@@ -2,8 +2,9 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 8
 @export var sensitivity = -0.1
+var capture = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -20,9 +21,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -31,11 +30,24 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		
+	if Input.is_action_just_pressed("ground_slam") and not is_on_floor():
+		gravity += 500
+	elif is_on_floor():
+		gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+		
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		capture = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		capture = false
 	move_and_slide()
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and capture == true:
 		rotate_y(deg_to_rad(event.relative.x * sensitivity))
 		$head.rotate_x(deg_to_rad(event.relative.y * sensitivity))
+		$head.rotation.x = clamp($head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
 	
