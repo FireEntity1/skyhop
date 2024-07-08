214292f8b1fdd2f1680d0fa830226377b203f415
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-07-07 23:09:19 -0600

    More levels

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 15
+var speed = 15
 const JUMP_VELOCITY = 10
 @export var sensitivity = -0.1
 var capture = true
+var jumps = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -18,22 +19,25 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jumps == 1:
 		velocity.y = JUMP_VELOCITY
+		jumps = 0
+
 
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 		
 	if Input.is_action_just_pressed("ground_slam") and not is_on_floor():
 		gravity += 500
 	elif is_on_floor():
 		gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+		jumps = 1
 		
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		capture = true
@@ -55,12 +59,10 @@ func _input(event):
 		$head.rotate_x(deg_to_rad(event.relative.y * sensitivity))
 		$head.rotation.x = clamp($head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
-	
-
 
 func _on_end_point_body_entered(body):
-		
 	if body is CharacterBody3D:
 		print("END")
 		match get_tree().current_scene.name:
-			"Tutorial" : print("Tutorial")
+			"Tutorial" : get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+			"Level1" : get_tree().change_scene_to_file("res://Scenes/Level2.tscn")
