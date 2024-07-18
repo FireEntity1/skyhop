5118b2f8d9a9a054fa397683a25deec7d4dbe70b
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-07-18 15:39:45 -0600

    Audio + Bug Fixes

--- Scenes/BossScene.gd
+++ Scenes/BossScene.gd
@@ -4,15 +4,11 @@ var index = 0
 var index2 = 0
 signal run
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_timer_timeout():
 	index += 1
@@ -20,14 +16,13 @@ func _on_timer_timeout():
 	match index:
 		1 : self.text = "You've  made it clear you don't want to cooperate"
 		2 : self.text = "And I won't either"
-		3 : self.text = ". . ."
-		4 : self.text = "D I E"; emit_signal("run")
-
+		3 : self.text = ". . ."; $Intro.stop()
+		4 : self.text = "D I E"; emit_signal("run"); $Final.play()
 
 func _on_end_point_body_entered(body):
 	if body is CharacterBody3D:
 		$Timer2.start(2)
-
+		$Final.stop()
 
 func _on_timer_2_timeout():
 	index2 += 1
